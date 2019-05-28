27cd558d8719110749cca6c99b1d86b1867cc700
Addicus Patton <addicus@gmail.com>
2019-05-28 11:10:17 -0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginRegComponent } from './login-reg/login-reg.component';
+import { UserComponent } from './user/user.component';
+import { SongListComponent } from './song-list/song-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginRegComponent,
+    UserComponent,
+    SongListComponent
   ],
   imports: [
     BrowserModule,
